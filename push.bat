d0a683a7fee1bc5456070cdc276317a3ee410af7
babymaxMAX <babymaxMAX@gmail.com>
2026-02-24 05:59:07 +0100

    feat: full Russia city proximity map ~250 cities

--- push.bat
+++ push.bat
@@ -2,6 +2,6 @@
 chcp 65001 >nul
 cd /d "%~dp0"
 git add -A
-git commit -m "feat: photo tap nav, geo proximity, hide profile, match check, photo likes notifications"
+git commit -m "feat: full Russia city proximity map ~250 cities"
 git push
 pause
